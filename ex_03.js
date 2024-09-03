bc6799ad81e8da0d18e8d605f046658c5b095250
Julien-bl <julien.bulin@epitech.digital>
2024-09-03 16:01:17 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,16 @@
+function changebackground() {
+var magicNumber = 1
+  if (magicNumber === 1) {
+    paintYellowRectangle(); magicNumber = 2
+  }
+
+  if (magicNumber === 2) { 
+    paintGreenRectangle(); magicNumber = 3
+  }
+ 
+  if (magicNumber === 3) {
+    paintRedRectangle();
+  }
+}
+
+changebackground();
