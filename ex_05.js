a1ba01f61134e1585be3c33a74605639c084f96f
Julien-bl <julien.bulin@epitech.digital>
2024-09-04 10:47:16 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,6 @@
+function getBasketContent() {
+const fruits = ["strawberry", "strawberry", "strawberry", "apple", "apple","lime", "lime", "peach", "pear", "pear"];
+console.log(`${fruits.lenght} fruit(s) seleted`);
+return fruits
+
+}
